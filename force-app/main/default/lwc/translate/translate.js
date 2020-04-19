fd966ac128efce9bab60490fe98531413a4af27b
NadiaRecarey <nadiarecareym@gmail.com>
2020-04-19 15:57:23 -0300

    Fixes structure

---
+++ force-app/main/default/lwc/translate/translate.js
@@ -0,0 +1,38 @@
+import { LightningElement, track } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import getTranslatedText from '@salesforce/apex/GoogleTranslateHelper.getTranslatedText';
+
+
+export default class BasicDatatable extends LightningElement {
+
+    text = [];
+
+    @track translatedText = '';
+
+    populateText(event) {
+        this.text = [];
+        this.text.push(event.detail.value);
+    }
+
+    translate() {
+
+        if (this.text) {
+
+            getTranslatedText({ texts: this.text})
+                .then(result => {
+                    if (result.length !== 0) {
+                        let dataClone = JSON.parse(JSON.stringify(result));
+                        this.translatedText = dataClone;
+                    }                     
+                })
+                .catch(error => {
+                    console.error('error message: ', error)
+                    const event = new ShowToastEvent({
+                        title: 'Error!',
+                        message: 'Something went wrong and we could not translate the texts.',
+                    });
+                    this.dispatchEvent(event);
+                });
+        }
+    }
+} 
